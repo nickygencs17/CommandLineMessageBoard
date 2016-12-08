a3531d9589629e3c11f54461922d48328b468f0e
Nicholas <Nicholas.Genco@stonybrook.edu>
2016-12-08 14:12:22 -0500

    changed client

--- src/Client.java
+++ src/Client.java
@@ -1,35 +1,31 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
-public class Client
-{
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class Client {
 
     private static Socket socket;
 
 
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         boolean loop = true;
         boolean socketcreated = false;
 
-        try
-        {
+        try {
             OutputStream os;
             OutputStreamWriter osw;
             BufferedWriter bw = null;
-            while(loop) {
+            while (loop) {
                 String host = "localhost";
                 int port = 1234;
                 InetAddress address = InetAddress.getByName(host);
-                if(!socketcreated) {
+                if (!socketcreated) {
                     socket = new Socket(address, port);
                     os = socket.getOutputStream();
                     osw = new OutputStreamWriter(os);
@@ -49,40 +45,142 @@ public class Client
                 InputStream is = socket.getInputStream();
                 InputStreamReader isr = new InputStreamReader(is);
                 BufferedReader br = new BufferedReader(isr);
-                String message ="";
+                String message = "";
                 String line = "";
-                while((line = br.readLine()) != null)
-                {
-                    if(line.equals("end"))
-                    {
+                while ((line = br.readLine()) != null) {
+                    if (line.equals("end")) {
                         break;
                     }
-                    if(line.equals("logout")) { socketcreated = false; socket.close();}
+                    if (line.equals("logout")) {
+                        socketcreated = false;
+                        socket.close();
+                    }
                     message += line + "\n";
                 }
-                System.out.println("Message received from the server : " + message);
-                if(message.equals("logout")){
+                String newMessage = readMessage(message);
+
+
+                System.out.println("Message received from the server : " + newMessage);
+                if (message.equals("logout")) {
                     loop = false;
                 }
             }
 
-        }
-        catch (Exception exception)
-        {
+        } catch (Exception exception) {
             exception.printStackTrace();
-        }
-        finally
-        {
+        } finally {
             //Closing the socket
-            try
-            {
+            try {
                 socket.close();
-            }
-            catch(Exception e)
-            {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
 
+    public static String readMessage(String message) {
+        String returnMessage = "";
+        JSONParser parser = new JSONParser();
+
+
+        try {
+            Object obj = parser.parse(message);
+
+            JSONObject jsonObject = (JSONObject) obj;
+            String s = jsonObject.get("type").toString();
+
+            if (s.equals("rgp")) {
+                returnMessage = readPost(message);
+            } else {
+                returnMessage = jsonObject.get("message").toString();
+            }
+
+        } catch (Exception v) {
+            System.out.println(v);
+        }
+
+        return returnMessage;
+
+
+    }
+
+    public static String readPost(String message) throws IOException {
+
+        String returnMessage = "";
+        JSONParser parser = new JSONParser();
+        String group = "";
+        String author = "";
+        SimpleDateFormat form = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        Date now = Calendar.getInstance().getTime();
+        String datestring = form.format(now);
+
+
+        try {
+            Object obj = parser.parse(message);
+
+            JSONObject jsonObject = (JSONObject) obj;
+            group = jsonObject.get("group").toString();
+            author = jsonObject.get("author ").toString();
+
+        } catch (Exception v) {
+            System.out.println(v);
+        }
+
+        System.out.println("Subject:");
+        String subject = "";
+
+        Scanner stdin = new Scanner(new BufferedInputStream(System.in));
+        while (stdin.hasNext()) {
+            subject += stdin.nextLine();
+        }
+        System.out.println("Post:");
+        String post = "";
+
+//        while (stdin.hasNext()) {
+//            {
+//                if (stdin.equals(".")) {
+//                    if (stdin.next().equals("\n")) {
+//                        break;
+//                    }
+//                }
+//            }
+//            else{
+//                post += stdin;
+//            }
+//
+//        }
+        BufferedReader stdin2 = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+
+        while ((line = stdin2.readLine()) != null && line.length() != 0) {
+            if (line.equals(".")) {
+                if (stdin2.readLine().equals("\n")) {
+                    break;
+                } else {
+                    post += line;
+
+
+                }
+
+            }
+
+
+        }
+        JSONArray array = new JSONArray();
+        array.add(author);
+
+        JSONObject sub = new JSONObject();
+        sub.put("subject", subject);
+        sub.put("group", group);
+        sub.put("author", author);
+        sub.put("post", post);
+        sub.put("time", datestring);
+        sub.put("viewers", array);
+
+        returnMessage = sub.toJSONString();
+
+
+        return returnMessage;
+
+    }
 }
